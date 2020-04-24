d1ae41c7e7ee14d09dfc1deb1c17c3732c25c6fc
smpleader <smpleader@gmail.com>
2020-04-24 13:23:40 +0700

    prepare to support method

--- src/router.php
+++ src/router.php
@@ -137,6 +137,64 @@ class Router extends staticObj
         }
 
         return ( $found === false ) ? $default : $found;
+    }
 
+    public function getRequestMethod()
+    {
+         $method = $_SERVER['REQUEST_METHOD'];
+
+         // If it's a HEAD request override it to being GET and prevent any output, as per HTTP Specification
+         // @url http://www.w3.org/Protocols/rfc2616/rfc2616-sec9.html#sec9.4
+         if ($_SERVER['REQUEST_METHOD'] == 'HEAD')
+         {
+            ob_start();
+            $method = 'GET';
+         }
+ 
+         // If it's a POST request, check for a method override header
+         elseif ($_SERVER['REQUEST_METHOD'] == 'POST')
+         {
+            $headers = $this->getRequestHeaders();
+            if (isset($headers['X-HTTP-Method-Override']) && in_array($headers['X-HTTP-Method-Override'], ['PUT', 'DELETE', 'PATCH']))
+            {
+                $method = $headers['X-HTTP-Method-Override'];
+            }
+
+            // well, support $_POST['_method']
+            if(isset($_POST['_method']) && in_array($_POST['_method'], ['PUT', 'DELETE', 'PATCH']))
+            {
+                $method = $_POST['_method'];
+            }
+
+         }
+ 
+         return strtolower($method);
+    }
+
+    public function getRequestHeaders()
+    {
+        $headers = [];
+
+        if (function_exists('getallheaders'))
+        {
+            $headers = getallheaders();
+
+            // getallheaders() can return false if something went wrong
+            if ($headers !== false)
+            {
+                return $headers;
+            }
+        }
+
+        // Method getallheaders() not available or went wrong: manually extract 'm
+        foreach ($_SERVER as $name => $value) 
+        {
+            if ((substr($name, 0, 5) == 'HTTP_') || ($name == 'CONTENT_TYPE') || ($name == 'CONTENT_LENGTH')) 
+            {
+                $headers[str_replace([' ', 'Http'], ['-', 'HTTP'], ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+            }
+        }
+
+        return $headers;
     }
 }
