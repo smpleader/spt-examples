623fc16187b0784e5a39efa65e4a9b0ce4400bbc
Minh Pham <smpleader@gmail.com>
2022-01-30 17:17:20 +0700

    remove files

--- examples/mvc/views/index.php
+++
@@ -1,9 +0,0 @@
-<html>
-    <head>
-        <title>Demo MVC</title>
-    </head>
-    <body>
-        This is site structure page. <hr/>
-        <?php echo $this->theme->getBody(); ?>
-    </body>
-<html>
